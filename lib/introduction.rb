fbb55e5237c7de284c4f611b80d49f7b310fd4df
Matthew Kindt <matthewkindt@yahoo.com>
2020-08-31 19:53:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language = "Python")
+def introduction_with_language_optional(name = "Edwin", language = "Python")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 introduction_with_language_optional("Edwin", "Python")
